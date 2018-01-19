bd52461fbf58e044b535fbf0b2f6c3dfc9f8dd27
MashaPo <ponomarevamawa@gmail.com>
2018-01-19 14:17:40 +0300

    readme encoding update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ def readme():
         return f.read()
 
 setup(name='russtress',
-      version='0.1',
+      version='0.1.1',
       description='Package that helps you to put lexical stress in russian text',
       long_description=readme(),
       url='https://github.com/MashaPo/russtress',
@@ -16,15 +16,15 @@ setup(name='russtress',
         'Topic :: Text Processing :: Linguistic',
         'Intended Audience :: Education'
       ],
-      author='Maria Ponomareva, Kirill Milintsevich'
-      keywords=' nlp russian stress linguistic',
+      author='Maria Ponomareva, Kirill Milintsevich',
+      keywords='nlp russian stress linguistic',
       author_email='ponomarevamawa@gmail.com',
       packages=['russtress'],
       install_requires=[
-          'Keras==2.0.4',
-		  'numpy==1.13.3',
-		  'scipy==0.19.0',
-		  'tensorflow==1.1.0'
+          'Keras',
+		  'numpy',
+		  'scipy',
+		  'tensorflow'
       ],
       include_package_data=True,
       zip_safe=False)
